31da1201b25aa9f739dd25629fbcebb842d3feb6
ashutoshdubey2508 <ashutoshdubey2508@gmail.com>
2023-07-14 12:05:36 +0530

    This is first time

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import RegisterView
+from .views import LoginView
+from .views import UserView
+from .views import LogoutView
+
+urlpatterns = [
+    path('register', RegisterView.as_view()),
+    path('login', LoginView.as_view()),
+    path('user',UserView.as_view()),
+    path('logout',LogoutView.as_view())
+]
